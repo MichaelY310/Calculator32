848951ee6ca0dc448565339f0e6efec20b3a4723
MichaelY310 <95990939+MichaelY310@users.noreply.github.com>
2023-10-16 15:46:53 -0700

    Update parse.cpp

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,6 +1,68 @@
 #include "lib/parse.h"
 
 
+
+
+
+
+void errorCheck(std::vector<Token> expression) {
+    if (expression.size() == 1 || !(expression.at(0).type == TokenType::leftParenthesis || expression.at(0).type == TokenType::number)) {
+        std::cout << "Unexpected token at line " << expression.at(0).line << " column " << expression.at(0).index << ": " << expression.at(0).content << std::endl;
+        exit(2);
+    }
+    if (expression.at(0).type == TokenType::number) {
+        if (expression.size() != 2) {
+            std::cout << "Unexpected token at line " << expression.at(1).line << " column " << expression.at(1).index << ": " << expression.at(1).content << std::endl;
+            exit(2);
+        }
+        else {
+            return;
+        }
+    }
+
+    if (!(expression.at(1).type == TokenType::plus || expression.at(1).type == TokenType::minus || expression.at(1).type == TokenType::multiply || expression.at(1).type == TokenType::divide)) {
+        std::cout << "Unexpected token at line " << expression.at(1).line << " column " << expression.at(1).index << ": " << expression.at(1).content << std::endl;
+        exit(2);
+    }
+
+    int paraCheck = 1;
+    for (size_t i = 1; i < expression.size() - 1; i++) {
+        if (expression.at(i).content == "(") {
+            paraCheck += 1;
+            if (!(expression.at(i+1).type == TokenType::plus || expression.at(i+1).type == TokenType::minus || expression.at(i+1).type == TokenType::multiply || expression.at(i+1).type == TokenType::divide)) {
+                std::cout << "Unexpected token at line " << expression.at(i+1).line << " column " << expression.at(i+1).index << ": " << expression.at(i+1).content << std::endl;
+                exit(2);
+            }
+        }
+        if (expression.at(i).content == ")") {
+            paraCheck -= 1;
+        }
+
+        if (expression.at(i).type == TokenType::plus || expression.at(i).type == TokenType::minus || expression.at(i).type == TokenType::multiply || expression.at(i).type == TokenType::divide) {
+            if (expression.at(i - 1).type != TokenType::leftParenthesis) {
+                std::cout << "Unexpected token at line " << expression.at(i).line << " column " << expression.at(i).index << ": " << expression.at(i).content << std::endl;
+                exit(2);
+            }
+        }
+
+        if (paraCheck < 0) {          // check para is correct
+            std::cout << "Unexpected token at line " << expression.at(i).line << " column " << expression.at(i).index << ": " << expression.at(i).content  << std::endl;
+            exit(2);
+        }
+        if (paraCheck == 0 && i != expression.size() - 2) {
+            std::cout << "Unexpected token at line " << expression.at(i+1).line << " column " << expression.at(i+1).index << ": " << expression.at(i+1).content  << std::endl;
+            exit(2);
+        }
+    }
+    if (paraCheck != 0) {          // check para is correct
+        std::cout << "Unexpected token at line " << expression.at(expression.size() - 1).line << " column " << expression.at(expression.size() - 1).index << ": " << expression.at(expression.size() - 1).content << std::endl;
+        exit(2);
+    }
+}
+
+
+
+
 int main() {
     std::string input = "";
     std::string line;
@@ -68,7 +130,7 @@ int main() {
         return 1;
     }
 
-
+    errorCheck(TokenVector);
 
     // parenthesis check
     for (int i=0; i < (int)TokenVector.size(); i++)
