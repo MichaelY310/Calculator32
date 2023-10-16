c81ee497f72b69c67139ebf92dd61dd96f8f2d50
MichaelY310 <95990939+MichaelY310@users.noreply.github.com>
2023-10-16 16:10:43 -0700

    Update lex.h

--- src/lib/lex.h
+++ src/lib/lex.h
@@ -242,36 +242,5 @@ void Token::printLexer(std::vector<Token> TokenVector)
 void Token::printLexer(const std::string input) 
 {
     std::vector<Token> TokenVector = GenTokenVector(input);
-    // Check ERROR
-    Token lastToken = TokenVector.back();
-    if (lastToken.type == TokenType::error)
-    {
-        std::cout << "Syntax error on line " << lastToken.line << " column " << lastToken.index << "." << std::endl;
-        return;
-    }
-    int spaceNumber;
-    std::string content; // line or index to string
-    for (Token t : TokenVector)
-    {
-        // line
-        content = std::to_string(t.line);
-        spaceNumber = 4 - content.length();
-        for (int i=0; i<spaceNumber; i++)
-        {
-            std::cout << " ";
-        }
-        std::cout << content;
-        // index
-        content = std::to_string(t.index);
-        spaceNumber = 5 - content.length();
-        for (int i=0; i<spaceNumber; i++)
-        {
-            std::cout << " ";
-        }
-        std::cout << content;
-        std::cout << " " << " ";
-        // token value
-        std::cout << t.content << std::endl;;        
-    }
-    return;
+    Token::printLexer(TokenVector);
 }
