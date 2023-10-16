1df3182b0a092ef15561cbff642fc00843e32f0f
MichaelY310 <95990939+MichaelY310@users.noreply.github.com>
2023-10-16 10:50:23 -0700

    Create parse.cpp

---
+++ src/lib/parse.cpp
@@ -0,0 +1,45 @@
+#include "parse.h
+
+int main() {
+    std::string input = "";
+    std::string line;
+    
+    while(!std::cin.eof()) {
+        char c;
+        std::cin.get(c);
+        line = std::string(1, c);
+        input += line;
+    }
+    input = input.substr(0, input.size()-1);
+
+    std::vector<Token> TokenVector = Token::GenTokenVector(input);
+    // Check ERROR
+    Token lastToken = TokenVector.back();
+    if (lastToken.type == TokenType::error)
+    {
+        std::cout << "Syntax error on line " << lastToken.line << " column " << lastToken.index << "." << std::endl;
+        return 1;
+    }
+
+    
+    Node root = Parser::MakeTree(TokenVector, 0, TokenVector.size() - 2);
+    if (Parser::ErrorToken.type != TokenType::none)
+    {
+        std::cout << "Unexpected token at line " << Parser::ErrorToken.line << "  column  " << Parser::ErrorToken.index << ": " << Parser::ErrorToken.content << std::endl;
+        return 2;
+    }
+    // Parser::print(root);
+    
+    double result= Parser::calculate(root);
+    if (Parser::ErrorToken.type != TokenType::none)
+    {
+        std::cout << "Runtime error: division by zero." << std::endl;
+        return 3;
+    }
+
+    //Token::printLexer(TokenVector);
+    std::cout << result << std::endl;
+    
+    
+    return 0;
+}
