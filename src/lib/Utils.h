2b9c1589ce97ff7056cb6b0d058fd2058d7925db
Michael <yang335@cs.ucsb.edu>
2023-10-19 18:20:13 -0700

    Checkpoint2 partA finished

--- src/lib/Utils.h
+++ src/lib/Utils.h
@@ -3,10 +3,8 @@
 #define DEBUG 0
 
 
-"""
-git add *
-git commit -m 'Checkpoint2 partA finished'
-push origin Michael
-"""
+// git add *
+// git commit -m "Checkpoint2 partA finished"
+// git push origin Michael
 
 // g++ -Wall -Wextra -Werror -std=c++17 parse.cpp -o main lib/Parser.cpp lib/Lexer.cpp
